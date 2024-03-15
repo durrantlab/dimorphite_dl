83925d42708e1912a6386d1c346376a22910fcff
Alex Morehead <alex.morehead@gmail.com>
2024-03-15 13:45:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=["rdkit", "numpy", "scipy", "mpi4py", "setuptools"],
     packages=find_packages(),
     package_data={
-        "": ["*.smi", "dimorphite_dl/*.smi", "*.smarts", "dimorphite_dl/*.smarts", "training_data/*.json", "dimorphite_dl/training_data/*.json"]
+        "": ["*.smi", "*.smarts", "training_data/*.json"]
     },
     include_package_data=True,
 )
